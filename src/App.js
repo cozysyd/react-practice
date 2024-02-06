97d71b52583873e51b214f834f13c5bf1dd8aeeb
Sydney Spaulding <cozysydmedia@gmail.com>
2024-02-05 21:53:14 -0800

    debugged component

--- src/App.js
+++ src/App.js
@@ -31,14 +31,30 @@ function App() {
 
       <hr />
 
-      <ul>
-        {list.map(function(item) {
-          return <li key={item.url}>
-        })}
-      </ul>
+      <List />
+
     </div>
       
   );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function(item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  )
 }
 
 
